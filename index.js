be0e6909172f3bcbc52e00c6717be36648ddb2cc
mattgud <mattgude@iastate.edu>
2023-03-12 16:47:39 -0500

    3/12 updated

--- index.js
+++ index.js
@@ -9,14 +9,51 @@ fetch('data.json')
         console.log('error:' + err);
     })
 function appendData(data) {
-    let mainContainer = document.getElementById("grid-container");
-    for (let element of data["Games"]) {
-        let div = document.createElement("div");
-        div.innerHTML = `
-        <a href="${element["page"]}" class="image-link-container">
-                <img src="${element["img-url"]}" alt="${element["name"]}" class="image-link">
-        </a>
-        <p class="subtitle">${element["name"]}</p>`;
-        mainContainer.appendChild(div)
+
+    var page = location.pathname.split("/").slice(-1)
+    console.log( page );
+
+    if(page == "games.html"){
+        for (let element of data["Games"]) {
+            let div = document.createElement("div");
+            div.innerHTML = `
+            <a href="${element["page"]}" class="image-link-container">
+                    <img src="${element["img-url"]}" alt="${element["name"]}" class="image-link">
+            </a>
+            <p class="subtitle">${element["name"]}</p>`;
+            document.getElementById("grid-container").appendChild(div)
+        }
+    } else if(page == "movies.html") {
+        for (let element of data["Movies"]) {
+            let div = document.createElement("div");
+            div.innerHTML = `
+            <a href="${element["page"]}" class="image-link-container">
+                    <img src="${element["img-url"]}" alt="${element["name"]}" class="image-link">
+            </a>
+            <p class="subtitle">${element["name"]}</p>`;
+            document.getElementById("grid-container").appendChild(div)
+        }
+    } else if(page == "music.html"){
+        for (let element of data["Music"]) {
+            let div = document.createElement("div");
+            div.innerHTML = `
+            <a href="${element["page"]}" class="image-link-container">
+                    <img src="${element["img-url"]}" alt="${element["name"]}" class="image-link">
+            </a>
+            <p class="subtitle">${element["name"]}</p>`;
+            document.getElementById("grid-container").appendChild(div)
+        }
+
+    } else if(page == "cars.html"){
+        for (let element of data["Cars"]) {
+            let div = document.createElement("div");
+            div.innerHTML = `
+            <a href="${element["page"]}" class="image-link-container">
+                    <img src="${element["img-url"]}" alt="${element["name"]}" class="image-link">
+            </a>
+            <p class="subtitle">${element["name"]}</p>`;
+            document.getElementById("grid-container").appendChild(div)
+        }
     }
+    
 }
