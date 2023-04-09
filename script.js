80c2c78bd3b08e3c08462b87ba0dd92e3669ec3e
mattgud <mattgude@iastate.edu>
2023-04-08 20:07:00 -0500

    Update 4/8

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
- // This works
+//Need different add/minus functions
+function add (e){
+    //document.getElementById(e).innerHTML = "Quantity: " + ++e; 
+}
+
+
  function toggleDiv(divid)
  {
    varon = divid + 'on';
@@ -38,6 +43,8 @@ function checkRegex(){
             console.log(e);
             if(e.name.toLowerCase().includes(sValue)){
                  values[values.length] = e.id;
+
+
             }
             
 
@@ -48,59 +55,109 @@ function checkRegex(){
     return values;
 }
 
-
-function generateTable() {
-    let div = document.getElementById("grid");
-    div.innerHTML = "";
-    const tbl = document.createElement("table");
-    const tblBody = document.createElement("tbody");
-
-    //get ids of products to send
-    productNum = checkRegex();
-    console.log(productNum + " product num"); // ID values to display
-
-    if(productNum.length == 0){
-        return 0; //leave if no products found
-    }
+function searchBar(){
+    ids = ["mug","airfryer","toaster","juicer","blender","coffeemaker"];
+    let divOn =[];
+    let divOff =[];
 
 
+    sValue = document.getElementById('search').value.toLowerCase();
 
-    let rows = productNum.length == 6 ? 2 : 1; console.log(rows + " rows");
-    let cols = 3;
+    ids.forEach((e) =>{
+        if(sValue == ""){
+            divOn = ["mug","airfryer","toaster","juicer","blender","coffeemaker"];
+            divOff =[];
+        }
+        if(e.toLowerCase().includes(sValue)){
+            divOn.push(e);
+        } else{
+            divOff.push(e)
+        }
+    });
+    console.log("Div on = " + divOn + "\nDiv off = " + divOff);
+
+    divOn.forEach((e) =>{
+        document.getElementById(e).style.display = 'block';
+    });
+    divOff.forEach((e) =>{
+        document.getElementById(e).style.display = 'none';
+    });
+
+    // if(sValue.length == 0){
+    //     console.log("true");
+    //     values = [1,2,3,4,5,6];
+    //     return values;
+    // } else{
+    //     products.forEach((e) =>
+    //     {
+    //         console.log(e);
+    //         if(e.name.toLowerCase().includes(sValue)){
+    //              values[values.length] = e.id;
+
+
+    //         }
+            
+
+    //     });
+   // }
+}
+
+// function generateTable() {
+//     let div = document.getElementById("grid");
+//     div.innerHTML = "";
+//     const tbl = document.createElement("table");
+//     const tblBody = document.createElement("tbody");
+
+//     //get ids of products to send
+//     productNum = checkRegex();
+//     console.log(productNum + " product num"); // ID values to display
+
+//     if(productNum.length == 0){
+//         return 0; //leave if no products found
+//     }
+
+
+
+//     let rows = productNum.length == 6 ? 2 : 1; console.log(rows + " rows");
+//     let cols = 3;
   
     
-    // creating all cells
-    for (let i = 0; i < rows; i++) {
-      const row = document.createElement("tr");
+//     // creating all cells
+//     for (let i = 0; i < rows; i++) {
+//       const row = document.createElement("tr");
   
-      for (let j = 0; j < cols; j++) {
-        //TO DO
-        // 
-        // - Populate price
-        // - Populate desc
-        // - Populate name
+//       for (let j = 0; j < cols; j++) {
 
-        const cell = document.createElement("td");
-        img = document.createElement("img")
-        img.src = products[productNum[(i*3)+j]-1].image;
-        img.style.width = "100px";
-        img.style.height = "100px";
-        //img.size = 25;
-        h1 = document.createElement("h1");
-        text = ([productNum[(i*3)+j]-1].name);
-        h1.innerHTML = text;
+//         //TO DO
+//         // 
+//         // - Populate price
+//         // - Populate desc
+//         // - Populate name
 
-        cell.appendChild(img);
-        cell.appendChild(h1);
-        row.appendChild(cell);
+//         const cell = document.createElement("td");
+//         img = document.createElement("img");
+//         console.log("I : " + i + "\n J: " + j + "\n"); 
+//         img.src = products[productNum[(i*3)+j]-1].image;
+//         img.style.width = "100px";
+//         img.style.height = "100px";
+//         //img.size = 25;
+//         h1 = document.createElement("h1");
+//         h1.innerHTML = (products[productNum[(i*3)+j]-1].name);
+//         p = document.createElement("p")
+//         p.innerHTML = products[productNum[(i*3)+j]-1].desc;
+
+//         cell.appendChild(img);
+//         cell.appendChild(h1);
+//         cell.appendChild(p);
+//         row.appendChild(cell);
         
-      }
-      tblBody.appendChild(row);
-    }
-    tbl.appendChild(tblBody);
-    div.appendChild(tbl);
-    tbl.setAttribute("border", "2");
-  }
+//       }
+//       tblBody.appendChild(row);
+//     }
+//     tbl.appendChild(tblBody);
+//     div.appendChild(tbl);
+//     tbl.setAttribute("border", "2");
+// }
   
 
 var values = [];
