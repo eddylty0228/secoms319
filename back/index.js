7bc2637cdd0ec9c997bf4bdce5bf162accb440ce
mattgud <mattgude@iastate.edu>
2023-04-28 15:56:03 -0500

    update

--- back/index.js
+++ back/index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const Product = require("./dataSchema.js");
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
-app.use("/images", express.static("images"));
-mongoose.connect("mongodb://127.0.0.1:27017/reactdata", {
+mongoose.connect("mongodb://127.0.0.1:27017/reactdata",
+{
 dbName: "reactdata",
 useNewUrlParser: true,
 useUnifiedTopology: true,
-});
+}
+);
 const port = process.env.PORT || 4000;
 const host = "localhost";
 app.listen(port, () => {
