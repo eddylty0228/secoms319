9bc12639a379c99b570ba4de3a26e73e9a3a1bb5
eddylty0228 <lty228eddy@gmail.com>
2023-05-06 17:07:24 -0500

    backend init

---
+++ Final/backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const Product = require("./dataSchema.js");
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
+//todo change reactdata to other database
+mongoose.connect("mongodb://127.0.0.1:27017/reactdata", {
+  dbName: "reactdata",
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//todo port
+const port = process.env.PORT || 4000;
+const host = "localhost";
+app.listen(port, () => {
+  console.log(`App listening at http://%s:%s`, host, port);
+});
